01dce3235b4ef2915f572f721e79ce208374544e
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-05-09 19:59:25 +0200

    Implement block actions and mob spawn restrictions near spawn
    Added new configuration options in config.yml for preventing block breaking, block placing, and mob spawning near the spawn area. This was implemented by adding relevant event handlers in the SpawnTownDistance class, which check the configured options and the distance from the spawn before allowing these actions. New permission checks were also added in the event handlers to bypass these restrictions.

--- src/main/java/me/youhavetrouble/spawntowndistance/SpawnTownDistance.java
+++ src/main/java/me/youhavetrouble/spawntowndistance/SpawnTownDistance.java
@@ -1,9 +1,12 @@
 package me.youhavetrouble.spawntowndistance;
 
+import com.destroystokyo.paper.event.entity.PreCreatureSpawnEvent;
 import com.palmergames.bukkit.towny.event.TownPreClaimEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.joml.Vector2d;
 
@@ -13,6 +16,8 @@ public final class SpawnTownDistance extends JavaPlugin implements Listener {
     private Vector2d spawnTownChunk = new Vector2d(0, 0);
     private int unclaimableDistance = 0;
     private String cancelMessage = "You cannot claim a town this close to spawn";
+
+    private boolean preventBlockBreak, preventBlockPlace, preventMobSpawn;
 
     @Override
     public void onEnable() {
@@ -29,25 +34,45 @@ public final class SpawnTownDistance extends JavaPlugin implements Listener {
         );
         this.unclaimableDistance = getConfig().getInt("unclaimable-distance", 0);
         this.cancelMessage = getConfig().getString("cancel-message", "You cannot claim a town this close to spawn");
+
+        this.preventBlockPlace = getConfig().getBoolean("prevent-block-place", true);
+        this.preventBlockBreak = getConfig().getBoolean("prevent-block-break", true);
+        this.preventMobSpawn = getConfig().getBoolean("prevent-mob-spawns", true);
+
     }
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onClaim(TownPreClaimEvent event) {
-
         // ignore towny admins, they probably know what they're doing. Hopefully.
         if (event.getPlayer().hasPermission("towny.admin.claim")) return;
 
-        if (getDistanceBetweenChunks(
-                spawnTownChunk,
-                new Vector2d(
-                        event.getTownBlock().getX() >> 4,
-                        event.getTownBlock().getZ() >> 4)
-        ) < unclaimableDistance) {
-            event.setCancelled(true);
-            event.setCancelMessage(this.cancelMessage);
-        }
+        Vector2d eventChunk = new Vector2d(event.getTownBlock().getX() >> 4, event.getTownBlock().getZ() >> 4);
+        if (getDistanceBetweenChunks(spawnTownChunk, eventChunk) > unclaimableDistance) return;
+        event.setCancelled(true);
+        event.setCancelMessage(this.cancelMessage);
+    }
 
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onBlockBreakNearSpawn(BlockBreakEvent event) {
+        Vector2d eventChunk = new Vector2d(event.getBlock().getChunk().getX(), event.getBlock().getChunk().getZ());
+        if (getDistanceBetweenChunks(spawnTownChunk, eventChunk) > unclaimableDistance) return;
+        if (event.getPlayer().hasPermission("spawntowndistance.bypass.breaking")) return;
+        event.setCancelled(true);
+    }
 
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onBlockPlaceNearSpawn(BlockPlaceEvent event) {
+        Vector2d eventChunk = new Vector2d(event.getBlock().getChunk().getX(), event.getBlock().getChunk().getZ());
+        if (getDistanceBetweenChunks(spawnTownChunk, eventChunk) > unclaimableDistance) return;
+        if (event.getPlayer().hasPermission("spawntowndistance.bypass.placing")) return;
+        event.setCancelled(true);
+    }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onMobSpawnNearSpawn(PreCreatureSpawnEvent event) {
+        Vector2d eventChunk = new Vector2d(event.getSpawnLocation().getChunk().getX(), event.getSpawnLocation().getChunk().getZ());
+        if (getDistanceBetweenChunks(spawnTownChunk, eventChunk) > unclaimableDistance) return;
+        event.setCancelled(true);
     }
 
     /**
